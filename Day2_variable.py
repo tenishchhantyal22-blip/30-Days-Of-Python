1e95d80a4ca51865708c53d96fb7aa6e9dfaf767
Tenesh Chhantyal <tenishchhantyal22@gmail.com>
2026-08-11 12:43:23 +0545

    Update and rename D2_variable.py to Day2_variable.py

---
+++ Day2_variable.py
@@ -0,0 +1,62 @@
+# variable in python
+first_name = 'Tenish'
+last_name = 'Chhantyal'
+country = 'Nepal'
+city_name = 'Pokhara Nepal'
+age = 19
+year = 2007
+is_married = 'false'
+personal_information {
+    'First_name:' 'Tenish',
+    'last_name:' 'Chhantyal',
+    'Class:' 'Twelve' ,
+}
+
+# Declaring multiple variable in single line
+first_name, last_name, age, year, is_married = 'Tenish', 'Chhantyal', 'Nineteen', 'false'
+print(first_name, last_name, age, year, is_married)
+print('First Name: ', first_name)
+print('Second Name: ', second_name)
+print('Age: ', age)
+print('Year You born: ', year)
+
+# variable exericese: level 2
+print(type('Tenish'))
+print(type(frist_name))
+print(type(last_name))
+print(type(200))
+
+first_name = "Tenish"
+last_name = "Chhantyal"
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+add = num_one + num_two
+subtract = num_one - num_two
+multiply = num_one * num_two
+divide = num_one % num_two
+power = num_two ** num_one 
+print(add)
+print(subtract)
+print(multiply)
+print(divide)
+print(power)
+
+# Calculation about circles
+radius = 30
+from math import pi
+area_of_circle = pi * (radius**2)
+cicum_of_circle = 2*pi*radius
+print(area_of_circle)
+print(cicum_of_circle)
+
+# Input you value
+radius = float(input("Enter the value of radius: "))
+from math import pi
+area_of_circle = pi * (radius*2)
+print(area_of_circle)
+
+
+
