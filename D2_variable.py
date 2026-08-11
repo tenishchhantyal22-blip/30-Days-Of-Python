e1555b4a4e862b9fc79c1e2fbcb4d326297ba120
Tenesh Chhantyal <tenishchhantyal22@gmail.com>
2026-08-11 11:14:44 +0545

    Initialize variables and print personal information
    Added personal information and multiple variable declarations.

---
+++ D2_variable.py
@@ -0,0 +1,21 @@
+# variable in python
+first_name = 'Tenish'
+last_name = 'Chhantyal'
+country = 'Nepal'
+city_name = 'Pokhara Nepal'
+age = 19
+year = 2007
+is_married = 'false'
+personal_information {
+    'First_name:' 'Tenish',
+    'last_name:' 'Chhantyal',
+    'Class:' 'Twelve' ,
+}
+
+# Declaring multiple variable in single line
+first_name, last_name, age, year, is_married = 'Tenish', 'Chhantyal', 'Nineteen', 'false'
+print(first_name, last_name, age, year, is_married)
+print('First Name: ', first_name)
+print('Second Name: ', second_name)
+print('Age: ', age)
+print('Year You born: ', year)
